d4bd613dd21e38ecd80a9e2218bc672b23a09dd6
Noëllie Chatain Six <noellie.chatain@hotmail.com>
2024-07-23 17:19:22 +0200

    create Header component with basic layout

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header className="app-header flex flex-col bg-slate-200 h-[12vh] p-6 w-full justify-center items-center">
+      <h1 className="text-3xl">Madame Barbouille</h1>
+      <p>Zen Art • Colors • Fun Stuff</p>
+    </header>
+  );
+}
+
+export default Header;
