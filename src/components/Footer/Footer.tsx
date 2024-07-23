0029f15196bfd91f1c2e4db742f2015ae826c716
Noëllie Chatain Six <noellie.chatain@hotmail.com>
2024-07-23 17:19:44 +0200

    create Footer component with basic layout

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <header className="app-header flex flex-col bg-slate-200 h-[12vh] p-6 w-full justify-center items-center mt-auto">
+      <p>2024</p>
+    </header>
+  );
+}
+
+export default Footer;
